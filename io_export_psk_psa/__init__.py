e51013eec7d9b164368d45333448c59fb8a5c703
Colin Basnett <cmbasnett@gmail.com>
2021-08-14 16:39:23 -0700

    Incremented version.

--- io_export_psk_psa/__init__.py
+++ io_export_psk_psa/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "PSK/PSA Exporter",
     "author": "Colin Basnett",
-    "version": ( 1, 0, 0 ),
+    "version": ( 1, 1, 0 ),
     "blender": ( 2, 80, 0 ),
     "location": "File > Export > PSK Export (.psk)",
     "description": "PSK/PSA Export (.psk)",
