bfdf1eb736a8d2bc5333fe717c79c73c9d6b45cb
Colin Basnett <cmbasnett@gmail.com>
2024-01-22 11:26:09 -0800

    PSK files imported with custom normals will now have `Auto Smooth` enabled by default (#67)

--- io_scene_psk_psa/psk/importer.py
+++ io_scene_psk_psa/psk/importer.py
@@ -227,6 +227,7 @@ def import_psk(psk: Psk, context, options: PskImportOptions) -> PskImportResult:
             for vertex_normal in psk.vertex_normals:
                 normals.append(tuple(vertex_normal))
             mesh_data.normals_split_custom_set_from_vertices(normals)
+            mesh_data.use_auto_smooth = True
         else:
             mesh_data.shade_smooth()
 
